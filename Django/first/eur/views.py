b4204b7aff70a1f42c0e5791d899ceb1364b1e29
MMM1381 <mehdimoazedy@gmail.com>
2024-10-13 18:16:01 +0330

    eur complete
    check out eurusd price

--- Django/first/eur/views.py
+++ Django/first/eur/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import yfinance as yf
 
 # Create your views here.
+def index(request):
+    data = yf.download(tickers='EURUSD=X', period='max', interval='1d')
+    close=data["Close"].iloc[0].item()
+    open=data["Open"].iloc[0].item()
+    high=data["High"].iloc[0].item()
+    low=data["Low"].iloc[0].item()
+    return render(request,"eur\price.html",{
+        "Open":open,
+        "High":high,
+        "Low":low,
+        "Close":close,
+        "ok":True
+    })
--- Django/first/first/urls.py
+++ Django/first/first/urls.py
@@ -19,5 +19,6 @@ from django.urls import path ,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',include("hello.urls"))
+    path('hello/',include("hello.urls")),
+    path("eur/",include("eur.urls"))
 ]
